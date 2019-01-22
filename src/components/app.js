326990260df0263bcb9ffff4155762ac14cd94ab
eidanfili <anning3232@gmail.com>
2019-01-22 14:29:30 -0700

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h2>eidan portfolio</h2>
+      </div>
+    );
+  }
+}
+
+
+
