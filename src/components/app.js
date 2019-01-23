0687681694c228a3a398c6cfdb1aabdeeb55de5f
eidanfili <anning3232@gmail.com>
2019-01-23 14:47:28 -0700

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import moment from "moment";
+
+import PortfolioContainer from './portfolio-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h2>eidan portfolio</h2>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
       </div>
     );
   }
 }
-
-
-
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class extends Component{
+    render(){
+    return(
+        <div>
+            <h2>Portfolio content</h2>
+        </div>
+        )
+    }
+}
